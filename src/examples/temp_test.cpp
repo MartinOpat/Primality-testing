a2f43afde6c571becf9614cb9a694b2d4e119a36
Martin Opat <martinopat.opat@gmail.com>
2025-09-26 18:31:57 +0200

    Test example now uses helpers

--- src/examples/temp_test.cpp
+++ src/examples/temp_test.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
-#include <stdio.h>
+
+#include "helpers.hpp"
 
 using namespace std;
-
-using lll = __int128;
-ostream &operator<<(ostream &o, __int128 n) {
-  auto t = n < 0 ? -n : n;
-  char b[128], *d = end(b);
-  do
-    *--d = '0' + t % 10, t /= 10;
-  while (t);
-  if (n < 0)
-    *--d = '-';
-  o.rdbuf()->sputn(d, end(b) - d);
-  return o;
-}
 
 int main() {
   __int128 a = 0;
