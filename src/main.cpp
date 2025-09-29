5f4232a9d2341ed58094d722e8679b731d916558
MartinOpat <martinopat.opat@gmail.com>
2025-09-29 18:50:04 +0200

    Updated main with miller rabin

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,34 @@
 #include <chrono>
 #include <iostream>
 
-#include "trialDivision/trial.hpp"
+#include "millerRabin/millerRabin.hpp"
 #include "primalityTest/primalityTest.hpp"
+#include "trialDivision/trial.hpp"
 
 #include "helpers.hpp"
 
-
-
 // TODO: Make a unified main for all tests that does the timing
 int main() {
-  PrimalityTest *pt = new TrialDivision();
-
+  // PrimalityTest *pt = new TrialDivision();
+  PrimalityTest *pt = new MillerRabin(1);
 
   const char *maxIntStr = "340000000000000000000000000000000000000";
   lll maxInt = strToInt(maxIntStr);
 
-  const char *aStr = "1000000";
+  const char *aStr = "100000000";
+  // const char *aStr = "34";
   lll a = strToInt(aStr);
 
-
   auto start = std::chrono::high_resolution_clock::now();
-  for (lll i = 0; i < a; i++) {
-    std::cout << i << " is a prime?";
-    std::cout << " Trial division says: " << (pt->isPrime(i) ? "yes" : "no") << std::endl;
+  for (lll i = 0; i < a; ++i) {
+    // std::cout << i << " is a prime?";
+    // std::cout << " Trial division says: " << (pt->isPrime(i) ? "yes" : "no")
+    // << std::endl;
+    pt->isPrime(i);
   }
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end - start;
-  std::cout << "Time ellapsed: " << elapsed.count() << "s" << std::endl; 
+  std::cout << "Time ellapsed: " << elapsed.count() << "s" << std::endl;
 
   delete pt;
 
