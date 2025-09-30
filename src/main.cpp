a3426c4ecf05b06cc8f96ef75227b087de84d26e
MartinOpat <martinopat.opat@gmail.com>
2025-09-30 14:27:56 +0200

    Added the necessary stuff to main to work with the bash script'

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <csignal>
 #include <iostream>
+#include <string>
 
 #include "millerRabin/millerRabin.hpp"
 #include "primalityTest/primalityTest.hpp"
@@ -8,36 +9,66 @@
 
 #include "helpers.hpp"
 
+// Global variables for efficient (async) info. logging
 lll i = 0;
+std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+PrimalityTest *pt = nullptr;
 
 void signal_handler(int signal) {
+  end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end - start;
   std::cout << "Reached: " << i << std::endl;
+  std::cout << "Duration: " << elapsed.count() << " s" << std::endl;
+  delete pt;
   std::exit(0);
 }
 
 // TODO: Make a unified main for all tests that does the timing
-int main() {
-
+int main(int argc, char *argv[]) {
   std::signal(SIGINT, signal_handler);
 
-  // PrimalityTest *pt = new TrialDivision();
-  PrimalityTest *pt = new MillerRabin(1);
+  std::string algPickStr = argv[1];
+  int algPick = std::stoi(algPickStr);
 
-  const char *maxIntStr = "340000000000000000000000000000000000000";
+  std::cout << "Algorithm picked: " << algPick << std::endl;
+
+  switch (algPick) {
+  case 0:
+    std::cout << "Picked Trial Division\n";
+    pt = new TrialDivision();
+    break;
+  case 1:
+    std::cout << "Picked Miller Rabin\n";
+    pt = new MillerRabin();
+    break;
+  case 2:
+    std::cout << "This is not implemented yet, stopping...\n";
+    return 0;
+  default:
+    std::cout << "Invalid algorithm pick, stopping...\n";
+    return 0;
+  }
+
+  const char *maxIntStr =
+      "34000000000000000"; // NOTE:Not the actual __128 limit, rather the limit
+                           // of how big ints we want to test
   lll maxInt = strToInt(maxIntStr);
+  std::cout << maxInt << std::endl;
 
-  const char *aStr = "100000000";
+  // const char *aStr = "100000000";
   // const char *aStr = "34";
-  lll a = strToInt(aStr);
+  // lll a = strToInt(aStr);
 
-  auto start = std::chrono::high_resolution_clock::now();
-  for (i = 0; i < a; ++i) {
+  start = std::chrono::high_resolution_clock::now();
+  for (i = 0; i < maxInt; ++i) {
     pt->isPrime(i);
   }
 
-  auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end - start;
   std::cout << "Time ellapsed: " << elapsed.count() << "s" << std::endl;
+  std::cout << "Warning: Code exited before being killed, consider increasing "
+               "max tested integer limit!"
+            << std::endl;
 
   delete pt;
 
