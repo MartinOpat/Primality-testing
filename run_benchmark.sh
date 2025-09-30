7346d615eace7d754f933200f454c2f3a65eb82e
MartinOpat <martinopat.opat@gmail.com>
2025-09-30 14:45:40 +0200

    Fixed bash script to actually kill the thing

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -10,14 +10,17 @@ fi
 
 declare -a tests=(0 1 2)
 
-# SIGINT is kill -2
-echo "Test log" >test.txt
+echo "test" >test.txt
 for i in "${tests[@]}"; do
-  ./PrimalityTesting $i | tee -a test.txt &
-  pid=$! # save process ID
+  # run the program, save its PID, redirect its output into tee
+  ./PrimalityTesting "$i" > >(tee -a test.txt) 2>&1 &
+  pid=$!
 
   sleep 3
-  if kill -0 $pid 2>/dev/null; then
-    kill -SIGINT $pid
+
+  if kill -0 "$pid" 2>/dev/null; then
+    kill -SIGINT "$pid"
   fi
+
+  wait "$pid" 2>/dev/null
 done
