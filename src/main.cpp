eccf52687df1c3f234ee3d0636430e8c2d59fbcc
MartinOpat <martinopat.opat@gmail.com>
2025-09-30 13:23:25 +0200

    Added signal handling for printing the last reached integer

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <csignal>
 #include <iostream>
 
 #include "millerRabin/millerRabin.hpp"
@@ -7,8 +8,18 @@
 
 #include "helpers.hpp"
 
+lll i = 0;
+
+void signal_handler(int signal) {
+  std::cout << "Reached: " << i << std::endl;
+  std::exit(0);
+}
+
 // TODO: Make a unified main for all tests that does the timing
 int main() {
+
+  std::signal(SIGINT, signal_handler);
+
   // PrimalityTest *pt = new TrialDivision();
   PrimalityTest *pt = new MillerRabin(1);
 
@@ -20,12 +31,10 @@ int main() {
   lll a = strToInt(aStr);
 
   auto start = std::chrono::high_resolution_clock::now();
-  for (lll i = 0; i < a; ++i) {
-    // std::cout << i << " is a prime?";
-    // std::cout << " Trial division says: " << (pt->isPrime(i) ? "yes" : "no")
-    // << std::endl;
+  for (i = 0; i < a; ++i) {
     pt->isPrime(i);
   }
+
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end - start;
   std::cout << "Time ellapsed: " << elapsed.count() << "s" << std::endl;
